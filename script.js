100f05084659a7758773a2d168c735998f0e3406
Vladislav Ayukaev <ayukaev@gmail.com>
2024-03-10 17:18:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
+
+            
         });
     });
+});
+
+
+document.getElementById('contact-btn').addEventListener('click', function() {
+    document.getElementById('contact-form-popup').classList.remove('hidden');
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('contact-form-popup').classList.add('hidden');
+});
+
+// Optional: Add functionality to prevent form submit for demonstration
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    alert('Form submitted!');
+    event.preventDefault(); // Prevent actual submit
+    document.getElementById('contact-form-popup').classList.add('hidden');
 });
